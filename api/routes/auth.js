7f080851c32218ba72c66af75b125a711ef54d7a
sentayhu19 <sentayhuberhanu19@gmail.com>
2022-01-02 20:01:22 +0300

    some updates and Jwt

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 const { Mongoose } = require("mongoose");
 const User = require("../models/user");
 var CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 router.post("/register", async (req, res) => {
+  //Encryption
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
@@ -23,11 +25,18 @@ router.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     !user && res.status(401).json("Wrong password or username");
+    //Decryption
     const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
     const originalText = bytes.toString(CryptoJS.enc.Utf8);
     originalText != req.body.password &&
       res.status(401).json("Wrong password or username");
-    res.status(200).json(user);
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user._isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+    const { password, ...info } = user._doc;
+    res.status(200).json({ ...info, accessToken });
   } catch (err) {
     res.status(500).json(err);
   }
